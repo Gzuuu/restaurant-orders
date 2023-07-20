0eef0962f83a2f855ff483bd20b482eedc7b4a21
Gzuuu <jesue_gege@hotmail.com>
2023-07-20 15:07:47 -0300

    requisito 1 concluido

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -3,4 +3,18 @@ from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient1 = Ingredient("bacon")
+    ingredient2 = Ingredient("salmão")
+    ingredient3 = Ingredient("bacon")
+    expected_restriction = {"ANIMAL_MEAT", "ANIMAL_DERIVED"}
+
+    assert not ingredient1 == ingredient2
+    assert ingredient1 == ingredient3
+    assert not hash(ingredient1) == hash(ingredient2)
+    assert hash(ingredient1) == hash(ingredient3)
+    assert ingredient1.name == "bacon"
+    assert repr(ingredient1) == "Ingredient('bacon')"
+    assert (
+        set(item.value for item in ingredient1.restrictions)
+        == expected_restriction
+    )
